3fa0c60e7cca1465033c4c0d2ba2a586c3c3077f
Carlos Villagrasa <carvillasil@gmail.com>
2022-09-14 21:56:56 +0200

    Update bundle

--- frontend/bundle.js
+++ frontend/bundle.js
@@ -121911,101 +121911,18 @@ const ifcExample5Button = document.getElementById("ifc-example5");
 const generateButton = document.getElementById("generate-button");
 const generatingBlock = document.getElementById("generating-block");
 const generatingText = document.getElementById("generating-txt");
-const timerRef = document.getElementById("timerDisplay");
+document.getElementById("timerDisplay");
 const container = document.getElementById("viewer-container");
 const unloadedMessage = document.getElementById("unloaded-message");
 const loadingAnimation = document.getElementById("loading-animation");
 
 let viewer = null;
 
-let [milliseconds, seconds, minutes] = [0, 0, 0];
-let timeElapsed = null;
-let timerStep = 100;
 
-
-function toggleGrid(bool_state) {
-    viewer.grid.grid.visible = bool_state;
-    viewer.axes.axes.visible = bool_state;
-}
-
-
-async function fetchWithTimeout(resource, options = {}) {
-    const { timeout = 200000 } = options;
-    const controller = new AbortController();
-    const id = setTimeout(() => controller.abort(), timeout);
-    const response = await fetch(resource, {
-        ...options,
-        signal: controller.signal
-    });
-    clearTimeout(id);
-    return response;
-}
-
-
-function requestStableDiffusionInpainting(prompt, image){
-    fetchWithTimeout("https://cvillagrasa.ddns.net:5000/receiver",
-        {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json',
-                'Accept': 'application/json'
-            },
-            //
-            body: JSON.stringify({
-                prompt,
-                image
-            })}).then(response => {
-        if(response.ok){
-            function handleResponse(response) {
-                return response.json();
-            }
-            handleResponse(response)
-                .then(json => json["imgInpainted"])
-                .then(imgInpainted => {
-                    document.getElementById("imgInpainted").src = imgInpainted;
-                    clearInterval(timeElapsed);
-                    generatingText.setAttribute("hidden", "");
-                });
-        }else {
-            if(timeElapsed !== null){
-                clearInterval(timeElapsed);
-                [milliseconds, seconds, minutes] = [0, 0, 0];
-                timeElapsed = null;
-            }
-            timerRef.innerHTML = "UNABLE TO PROCESS THE REQUEST";
-        }
-    }).catch((err) => console.error(err));
-}
-
-
-function inpaintingPipeline() {
-    const renderer = viewer.context.renderer.renderer;
-    toggleGrid(false);
-    viewer.context.render();
-    const imgData = renderer.domElement.toDataURL();
-    toggleGrid(true);
-    let data_fields = ["model-description", "context-description", "style-description"];
-    data_fields = data_fields.map(field => document.getElementById(field).value);
-    const prompt = data_fields[0] + " " + data_fields[1] + ". " + data_fields[2] + ".";
-    requestStableDiffusionInpainting(prompt, imgData);
-}
-
-
-function displayTimer(){
-    milliseconds+=timerStep;
-    if(milliseconds === 1000){
-        milliseconds = 0;
-        seconds++;
-        if(seconds === 60){
-            seconds = 0;
-            minutes++;
-        }
-    }
-
-    let m = minutes < 10 ? "0" + minutes : minutes;
-    let s = seconds < 10 ? "0" + seconds : seconds;
-    let ms = milliseconds / 100;
-    timerRef.innerHTML = `${m}' ${s}'' ${ms}`;
+function offlinePipeline() {
+    generatingBlock.removeAttribute("hidden");
+    generatingText.removeAttribute("hidden");
+    generatingText.innerHTML = "Sorry, the backend server is currently offline";
 }
 
 
@@ -122055,16 +121972,17 @@ ifcExample5Button.onclick = () => loadExampleFunctions[4]();
 
 
 generateButton.onclick = () => {
-    if (viewer === null) {
-        alert("Please, load an IFC file before!");
-        return
-    }
-    generatingBlock.removeAttribute("hidden");
-    generatingText.removeAttribute("hidden");
-    if(timeElapsed !== null){
-        clearInterval(timeElapsed);
-        [milliseconds, seconds, minutes] = [0, 0, 0];
-    }
-    timeElapsed = setInterval(displayTimer, timerStep);
-    inpaintingPipeline();
+    // if (viewer === null) {
+    //     alert("Please, load an IFC file before!")
+    //     return
+    // }
+    // generatingBlock.removeAttribute("hidden");
+    // generatingText.removeAttribute("hidden");
+    // if(timeElapsed !== null){
+    //     clearInterval(timeElapsed);
+    //     [milliseconds, seconds, minutes] = [0, 0, 0];
+    // }
+    // timeElapsed = setInterval(displayTimer, timerStep);
+    // inpaintingPipeline();
+    offlinePipeline();
 };
